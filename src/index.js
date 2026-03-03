3ca2a61bbacc6c7edb7927c03bbcc8a6e8bcf816
AMN-47 <akhilnagar57@gmail.com>
2026-03-03 10:52:21 +0200

    Too much broken code, fixing errors resulted in more errors, cycle of errors, restarting

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import { loadState } from './todo-logic';
-import { initDom } from './initial-dom-manip';
-
-console.log("Testing initialasaiton of index,js");
-
-
-loadState();
-initDom();
