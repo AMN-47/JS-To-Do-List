63b6a59315a9b95f82a4522067fc299e5e5ba03f
AMN-47 <akhilnagar57@gmail.com>
2026-03-04 13:51:39 +0200

    added logic for standard strings for todo list

--- src/todologic.js
+++ src/todologic.js
@@ -2,5 +2,10 @@ function createTodo(title, description, dueDate, priority, notes) {
     return {
         title: title || "Untitled",
         description: description || "",
-    }
+        dueDate: dueDate || "",
+        priority: priority || "medium",
+        notes: notes || "",
+        completed: false, //standard for all todos to start as incomplete
+    };
 }
+
