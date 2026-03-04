8aa6db361950ab26cd983bd8084286bfb0db0182
AMN-47 <akhilnagar57@gmail.com>
2026-03-04 15:00:52 +0200

    coded createProject function

--- src/todologic.js
+++ src/todologic.js
@@ -9,3 +9,18 @@ function createTodo(title, description, dueDate, priority, notes) {
     };
 }
 
+//function to display the name of the project
+function createProject(name) {
+    return {
+        name: name || "Untitled Project",
+        todos: [],
+    };
+}
+
+//App State
+//App State function is where central object stores everything that needs to be remembered in that moment
+
+const appState = {
+    projects: [], //Lists the project the user created
+    activeIndex: -1, //current project selected with -1 meaning no project selected
+};
