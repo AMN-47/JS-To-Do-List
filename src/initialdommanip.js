00639ff2e1f30ce87eba3089c6c6c3e2eb6e11fc
AMN-47 <akhilnagar57@gmail.com>
2026-03-05 14:30:18 +0200

    Added layout builder function

--- src/initialdommanip.js
+++ src/initialdommanip.js
@@ -11,4 +11,28 @@ import {
     loadFromLocalStorage,
     seedDefualtData,
     getTodayDateString,
-} from './todologic.js'
+} from './todologic.js'; //Bringing in logic to work on elements
+
+import "./style.css"; // importing stylesheet to work on page during runtime 
+
+//Bui;ding the skeleton 
+
+function buildLayout() {
+    const content = document.getElementById("content");
+
+
+    //Side Bar
+    const sidebar = document.createElement("aside");
+    sidebar.id = sidebar;
+
+    //Main content
+    const main = document.createElement("main");
+    main.id = "main-content";
+
+
+    content.appendChild(sidebar);
+    content.appendChild(main);
+
+    return {sidebar, main};
+}
+
